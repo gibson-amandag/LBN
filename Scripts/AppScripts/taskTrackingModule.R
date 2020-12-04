77be5e7b2e03a6aa7d798d1ae28d79406087998a
Amanda Gibson <gibsonag@umich.edu>
2020-12-04 16:17:51 -0500

    Changed list style to open circle, dropped checkbox

--- Scripts/AppScripts/taskTrackingModule.R
+++ Scripts/AppScripts/taskTrackingModule.R
@@ -203,9 +203,9 @@ taskTrackingServer <- function(id){
             select(Dam_ID, mass_P10:mass_P19) %>%
             filter_all(any_vars(. %in% Day))
           if(nrow(mass_on_date_litter) > 0){ #only print if there are values in df
-            printCat <- list_add(printCat, paste0("<em>Take the ", blueText("mass"), " of the following litters:</em> <ul>"))
+            printCat <- list_add(printCat, paste0("<em>Take the ", blueText("mass"), " of the following litters:</em> <ul style=\"list-style-type:circle;\">"))
             for(val in seq_along(mass_on_date_litter$Dam_ID)){
-              printCat <- list_add(printCat, paste0("<li><input type=\"checkbox\">", mass_on_date_litter$Dam_ID[val], "</li>"))
+              printCat <- list_add(printCat, paste0("<li>", mass_on_date_litter$Dam_ID[val], "</li>"))
             }
           }
           if(nrow(mass_on_date_litter) > 0){printCat <- list_add(printCat, "</ul>")}
@@ -216,9 +216,9 @@ taskTrackingServer <- function(id){
             select(Mouse_ID, mass_P22:mass_P72) %>%
             filter_all(any_vars(. %in% Day))
           if(nrow(mass_on_date) > 0){ #only print if there are values in df
-            printCat <- list_add(printCat, paste0("<em>Take the ", blueText("mass"), " of the following offspring:</em> <ul>"))
+            printCat <- list_add(printCat, paste0("<em>Take the ", blueText("mass"), " of the following offspring:</em> <ul style=\"list-style-type:circle;\">"))
             for(val in seq_along(mass_on_date$Mouse_ID)){
-              printCat <- list_add(printCat, paste0("<li><input type=\"checkbox\">", mass_on_date$Mouse_ID[val], "</li>"))
+              printCat <- list_add(printCat, paste0("<li>", mass_on_date$Mouse_ID[val], "</li>"))
             }
           }
           if(nrow(mass_on_date) > 0){printCat <- list_add(printCat, "</ul>")}
--- Scripts/Functions/LBN_0002_AGG_taskFunctions.R
+++ Scripts/Functions/LBN_0002_AGG_taskFunctions.R
@@ -230,14 +230,14 @@ Dam_tasks_app <- function(
     #add the text to print to the list
     list_name <- list_add(list_name, paste0("<em>", #make this text emphasized
                                             task_text, ": </em>",#end emphasis
-                                            "<ul>" #start a list
+                                            "<ul style=\"list-style-type:circle;\">" #start a list
     ))
     Count <<- 1
   }
   #add the dam id to the list and wrap it in instructions to make it a bullet point
   #the end of the list instruction is in the printLine_func_app function
   #add a checkbox for each item
-  list_name <- list_add(list_name, paste0("<li> <input type=\"checkbox\">", df[[id_var]][val], "</li>"))
+  list_name <- list_add(list_name, paste0("<li>", df[[id_var]][val], "</li>"))
   return(list_name)
 }
 
@@ -255,12 +255,12 @@ Off_tasks_app <- function(
     #Then change the counter to 1
     list_name <- list_add(list_name, paste0("<em>", #make this text strong
                                             task_text, ": </em>",#end strong
-                                            "<ul>"#start a list
+                                            "<ul style=\"list-style-type:circle;\">"#start a list
     ))
     Count <<- 1
   }
   #Add the Mouse_ID to the list wrapped in html instructions to make it a bullet point
-  list_name <- list_add(list_name, paste0("<li><input type=\"checkbox\">", df[[id_var]][val], "</li>"))
+  list_name <- list_add(list_name, paste0("<li>", df[[id_var]][val], "</li>"))
   return(list_name)
 }
 
