ee467eb7f7f647f4a64c406ff524811cb6e448f6
Amanda Gibson <gibsonag@umich.edu>
2021-08-24 13:12:33 -0400

    Change myXLSX_func to loadExcelSheet

--- 02-functions/01-get-data-ready/load-data.R
+++ 02-functions/01-get-data-ready/load-data.R
@@ -1,5 +1,20 @@
 ### Loading Data Sets-----------------------------------------------------------------------------------------
-myXLSX_func = function(folderPath, fileName, sheetName){
+#' Load an excel sheet as a dataframe
+#' 
+#' Uses the openxlsx package function read.xlsx to read the excel sheet
+#' The first row is used as column names. These should not have spaces
+#' NA cells in excel should be "NA" string
+#' Dates are detected, empty rows and columns are skipped
+#'
+#' @param folderPath folder path where excel file is saved
+#' @param fileName excel file name - include extension
+#' @param sheetName name of sheet you want to load
+#'
+#' @return a dataframe with the data from the sheet
+#' @export
+#'
+#' @examples
+loadExcelSheet = function(folderPath, fileName, sheetName){
   read.xlsx(
     file.path(folderPath, fileName),
     sheet = sheetName,
